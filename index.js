3e30bd3a899afc1d3026b8e69c437349f2857d36
Edgar Damatian <damatian.e@gmail.com>
2024-01-30 11:39:08 -0800

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function() {
+    const result = 2000 / 100;
+    return result;
+  };
+
+  const square = (number) => {
+    return number * number;
+  };
+
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+
+const divisionResult = divide();
+console.log(divisionResult);
